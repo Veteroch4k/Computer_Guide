ca45a191e9ae2b269cca1fdf0d738e1cc0036413
Veteroch4k <indefinite05@mail.ru>
2023-10-23 18:52:56 +0700

    Initial commit

---
+++ src/main/java/com/veteroch4k/org/ComputerGuideApplication.java
@@ -0,0 +1,13 @@
+package com.veteroch4k.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veteroch4k/org/ComputerGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veteroch4k.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
