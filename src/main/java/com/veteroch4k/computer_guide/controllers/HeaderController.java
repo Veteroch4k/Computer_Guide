8afa738a8146fd01bc17670dd2048177598a95d5
Veteroch4k <indefinite05@mail.ru>
2024-04-02 23:45:57 +0700

    Implemented transitions between header's pages

--- src/main/java/com/veteroch4k/computer_guide/controllers/HeaderController.java
+++ src/main/java/com/veteroch4k/computer_guide/controllers/HeaderController.java
@@ -1,5 +1,6 @@
 package com.veteroch4k.computer_guide.controllers;
 
+import jakarta.jws.WebParam.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,25 @@ public class HeaderController {
         modelAndView.setViewName("header-pages/aboutUs");
         return modelAndView;
     }
+
+    @GetMapping("/periphery")
+    public ModelAndView periphery(ModelAndView modelAndView) {
+        modelAndView.addObject("title", "Периферия");
+        modelAndView.setViewName("header-pages/periphery");
+        return modelAndView;
+    }
+
+    @GetMapping("/system-unit")
+    public ModelAndView system_unit(ModelAndView modelAndView) {
+        modelAndView.addObject("title", "Системный блок");
+        modelAndView.setViewName("header-pages/system-unit");
+        return modelAndView;
+    }
+
     @GetMapping("/testing-system")
     public ModelAndView testing(ModelAndView modelAndView) {
         modelAndView.addObject("title", "Система тестирования");
         modelAndView.setViewName("header-pages/testing-system");
         return modelAndView;
     }
-
 }
--- src/main/java/com/veteroch4k/computer_guide/controllers/TestingSystemController.java
+++ src/main/java/com/veteroch4k/computer_guide/controllers/TestingSystemController.java
@@ -8,4 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestingSystemController {
 
 
+
+
 }
